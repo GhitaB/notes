ceb1196fd74ebf7362a0734339eff0278157a86d
GhitaB <ghitabizau27@gmail.com>
2021-01-12 18:38:47 +0200

    Initial configuration.

--- notes.py
+++ notes.py
@@ -3,18 +3,33 @@
 
 from tkinter import *
 from tkinter import ttk
+import os.path
+from os import path
 
-STATUS_NEW = u"Status: The folder for your notes is configured."
+STATUS_FOLDER_EXISTS = u"Status: Configuration is ok."
+STATUS_FOLDER_SUCCESS = u"Status: The folder for notes was created."
+STATUS_FOLDER_FAIL = u"Status: Cannot create the folder for notes."
+FOLDER_NOTES = "./my-notes/"
 
 class NotesApp:
-    def configure_folder(self):
-        print("TODO configure folder")
+    def do_configuration(self):
+        if path.exists(FOLDER_NOTES):
+            msg = STATUS_FOLDER_EXISTS
+        else:
+            try:
+                os.makedirs(FOLDER_NOTES)
+                msg = STATUS_FOLDER_SUCCESS
+            except Exception:
+                msg = STATUS_FOLDER_FAIL
+
+        return msg
+
 
     def __init__(self, master):
 
-        self.configure_folder()
+        msg = self.do_configuration()
 
-        self.label_status = ttk.Label(master, text = STATUS_NEW)
+        self.label_status = ttk.Label(master, text = msg)
         self.label_status.grid(row = 0, column = 0, columnspan = 2)
 
         # ttk.Button(master, text = "Romania",
