c375086758432e4c8bc0caf86dc7d65f1e26a95f
GhitaB <ghitabizau27@gmail.com>
2021-01-12 18:22:43 +0200

    WIP initial config.

--- notes.py
+++ notes.py
@@ -4,23 +4,30 @@
 from tkinter import *
 from tkinter import ttk
 
+STATUS_NEW = u"Status: The folder for your notes is configured."
+
 class NotesApp:
+    def configure_folder(self):
+        print("TODO configure folder")
+
     def __init__(self, master):
 
-        self.label = ttk.Label(master, text = "Hello, World!")
-        self.label.grid(row = 0, column = 0, columnspan = 2)
+        self.configure_folder()
 
-        ttk.Button(master, text = "Romania",
-                   command = self.romania_hello).grid(row = 1, column = 0)
+        self.label_status = ttk.Label(master, text = STATUS_NEW)
+        self.label_status.grid(row = 0, column = 0, columnspan = 2)
 
-        ttk.Button(master, text = "USA",
-                   command = self.usa_hello).grid(row = 1, column = 1)
+        # ttk.Button(master, text = "Romania",
+        #            command = self.romania_hello).grid(row = 1, column = 0)
+        #
+        # ttk.Button(master, text = "USA",
+        #            command = self.usa_hello).grid(row = 1, column = 1)
 
-    def romania_hello(self):
-        self.label.config(text = 'Hello, Romania!')
-
-    def usa_hello(self):
-        self.label.config(text = 'Hi, USA!')
+    # def romania_hello(self):
+    #     self.label.config(text = 'Hello, Romania!')
+    #
+    # def usa_hello(self):
+    #     self.label.config(text = 'Hi, USA!')
 
 
 def main():
